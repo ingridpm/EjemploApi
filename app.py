f4f76e332911b69896d730ede5a2d1998d540e56
Ingrid <ingrid.ipcb@gmail.com>
2021-04-10 12:32:18 -0600

    rutas

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("hola mundo")
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def pantalla_principal():
+    return "Prueba"
+
+@app.route('/hola',methods=['GET'])
+def hola_mundo():
+    return "hola mundo"
+
+if __name__ == '__main__':
+    puerto = 5000
+    app.run(host='0.0.0.0', port = puerto)
